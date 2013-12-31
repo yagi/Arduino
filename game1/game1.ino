e7b3ecf27f97e99af4b6bcb9ff2d8a4d058bdd0f
Yagi Kusigahakama <patama@gmail.com>
2014-01-01 02:51:35 +0900

    アナログピンがデジタル扱いできないのよう

--- game1/game1.ino
+++ game1/game1.ino
@@ -1,5 +1,5 @@
 #include <Key.h>
-Key on(11), off(10), ok(13);
+Key on(11), off(10), ok(15);
 extern  short freq[];
 #include "Speaker.h"
 Speaker spk(12);
@@ -9,38 +9,18 @@ Bar bar;
 void demo() {
   for (int i=0 ; i<=20 ; ++i) {
     switch (i) {
-    case  0: spk.beep(0,30); break;
-    case  3: spk.beep(4,30); break;
-    case  6: spk.beep(7,30); break;
-    case 10: spk.beep(12,30); break;
-    case 13: spk.beep(7,60); break;
-    case 15: spk.beep(4,60); break;
-    case 18: spk.beep(0,60);  break;
+    case  0: spk.beep(0,30); break; case  3: spk.beep(4,30); break;
+    case  6: spk.beep(7,30); break; case 10: spk.beep(12,30); break;
+    case 13: spk.beep(7,60); break; case 15: spk.beep(4,60); break;
+    case 18: spk.beep(0,60); break; default: break;
     }
     char pin = (i<=10) ? i : (20-i);    
-    bar.on(pin);
-    delay(20);
-    bar.off(pin);
+    bar.on(pin); delay(20); bar.off(pin);
   }  
 }
 
-
-static char notes[][2] = {
-  {1,2},{3,4},{6,4},{2,1}
-};
-// 音の高さ CDEF - 音の長さ 
-
-class Game {
-  void beep(char code, char len) { }  
-  bool handleUp() {  }  
-};
-
-
-void dispNumber(int n) {
-}
-
 void setup() {  
-  //spk.playMml("CDEFGABC");
+  //spk.playMml("CDEFGABC"); delay(100);
   demo();
 }
 
